c92a6a4568680309fdacc58e98ffcb9a092616ca
emmanesgana <emman.esgana@gmail.com>
2024-05-12 06:41:18 +0800

    code update
    Updated html, css, js and image files

--- src/js/data.js
+++ src/js/data.js
@@ -1,24 +1,27 @@
 const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: ["pepperoni", "mushroom", "mozarella"],
         price: 14,
-        emoji: "🍕",
-        id: 0
+        image: "./src/img/pizza.png",
+        id: 0,
+        quantity: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
-        id: 1
+        image: "./src/img/burger.png",
+        id: 1,
+        quantity: 0
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        emoji: "🍺",
-        id: 2
+        image: "./src/img/beer.png",
+        id: 2,
+        quantity: 0
     }
 ]
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,162 @@
 // import function from data.js using default export
 import menuArray from "/src/js/data.js"
 
-// creatae a function do render menu html
-function renderMenuHtml(menuList){
+const menuSectionEl = document.getElementById('menu-section')
+const cartSectionEl = document.getElementById('cart-section')
+const modalSectionEl = document.getElementById('modal')
 
-    return menuList.map(menu => {
+const cartArray = []
+let totalPrice = 0
+
+// create an eventListener to check the id of the item to be added to checkout
+document.addEventListener('click', function(e){
+    if (e.target.dataset.add){
+        addItem(e.target.dataset.add);
+    }
+    else if (e.target.dataset.remove) {
+        removeItem(e.target.dataset.remove);
+    }
+})
+
+// create a function to display the menu items
+function renderMenuHtml(menu){
+    
+    //hide modal and cart section on load
+    cartSectionEl.style.display = 'none'
+    modalSectionEl.style.display = 'none'
+
+    // use .map to display each object array
+    return menu.map(menuItem => {
+        // use object destructuring
         const {
             name,
             ingredients,
             price,
-            emoji,
+            image,
             id
-        } = menu
-    
-        console.log(name)
+        } = menuItem
+        
+        // display array object as html
+        menuSectionEl.innerHTML += `
+            <div class="menu-item">
+                <img class="menu-item-image" src="${image}">
+                <div class="menu-item-info">
+                    <div class="item-name">${name}</div>
+                    <div class="item-ingredients">${ingredients.join(', ')}</div>
+                    <div class="item-price">$ ${price}</div>
+                </div>
+                <button data-add="${id}" class="add-button">+</button>
+            </div>
+        `
+    }).join('')
+}
 
+// create a function for add button
+function addItem(id) {
+
+    //remove item from cart using the id via filter method
+    const targetObj = menuArray.filter((item) => {
+      return item.id == id;
+    })[0];
+    
+    //add item to cart
+    if (!cartArray.includes(targetObj)) {
+        cartArray.push(targetObj);
+    }
+    
+    //create a loop to add quantity when pressing add button from menu section
+    cartArray.forEach((item) => {
+      if (item.id === targetObj.id) {
+        item.quantity++;
+      }
+    });
+    
+    //update quantity to totalPrice
+    totalPrice += targetObj.price;
+    
+    cartSectionEl.style.display = 'block'
+  
+    renderCartHtml();
+  }
+
+// create a function to remove item from cart
+function removeItem(id) {
+
+    //remove item from cart using the id via filter method
+    const targetObj = cartArray.filter((item) => {
+        return item.id == id
+    })[0]
+    
+    //create a loop to check items inside the cartArray
+    cartArray.forEach((item, idRemove) => {
+
+        //remove if quantity is not 0
+        if (item.id === targetObj.id) {
+            item.quantity--;
+        }
+
+        //remove entire item row if quantity is 0
+        if (item.quantity === 0) {
+            cartArray.splice(idRemove, 1)
+        }
     })
+  
+    //remove cart section is array is empty
+    if (cartArray.length === 0) {
+        cartSectionEl.style.display = 'none'
+    }
+    
+    totalPrice -= targetObj.price
+  
+    renderCartHtml();
+}
+
+// create a function to display the checkout section at the bottom
+function renderCart() {
+
+    let getCheckoutHtml = ``
+
+    cartArray.forEach((item) => {
+
+        getCheckoutHtml += `
+            <div class="cart-list" id="cart-list">  
+                <div class="cart-item-info">
+                    <h3>${item.name}</h3>
+                    <h4 id="cart-item-qty">x${item.quantity}</h4>
+                    <button id="remove-item-btn" data-remove="${item.id}">Remove</button>
+                </div>
+                <div class="item-price">
+                    <h4>$ ${item.price * item.quantity}</h4>
+                </div>
+            </div>
+        `
+    })
+  
+    let cartSectionHtml = ``
+
+    cartArray.forEach(() => {
+
+        cartSectionHtml = `
+            <div id="items-ordered">
+                <div>
+                    <h3 id="order-title">Your order</h3>
+                </div>
+                <div id="order-list">${getCheckoutHtml}</div>
+                <div class="order-separator"></div>
+                <div class="total-price">
+                    <div><p>Total: </p></div>
+                    <div><p id="total-price">$ ${totalPrice}</p></div>
+                </div>
+                <button id="complete-order-btn">Complete order</button>
+            </div>
+        `
+    })
+  
+    return cartSectionHtml;
+}
+
+function renderCartHtml(){
+    cartSectionEl.innerHTML = renderCart()
 }
 
 renderMenuHtml(menuArray)
